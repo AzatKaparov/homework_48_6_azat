940738395821dd665054257c798bb9dd84ceb942
Azat Kaparov <azatkaparov843@gmail.com>
2020-07-31 20:11:02 +0600

    Вторая миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 DEFAULT_CATEGORY = 'other'
@@ -18,7 +19,7 @@ class Product(models.Model):
     description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
     category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, null=False, blank=False,
                                 default=DEFAULT_CATEGORY, verbose_name='Категория')
-    amount = models.IntegerField(verbose_name='Остаток')
+    amount = models.IntegerField(verbose_name='Остаток', validators=[(MinValueValidator(0))])
 
     price = models.DecimalField(verbose_name='Цена', max_digits=7, decimal_places=2)
 
